cfeb4d1b80c6bd11c319ed46d5e93f447de0ee70
LucasAguirre1986 <Lucas.aguirre1986@gmail.com>
2019-04-09 19:20:20 -0300

    agregue comandos

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hola");
+# GIT
+
+## COMMAND
+	git init // Para iniciar git
+	git status // para revisar el status de los archivos
+	git add file // para agregar al stagin area
+     
+	git commit -m // para coomitear con un mensaje
+	git checout file // Volver al ultimo commit
+
+## PUSH
+	git init // Para iniciar
+	git remote -v // para ver las versiones
+	git remote add origin https://github.com/LucasAguirre1986/prep.git
+	git pull origin master // para importar la info de la web
+	git push origin master // Para subir los cambios
+	git clone https://github.com/LucasAguirre1986/prep // para clonar
+
+	echo 'mensaje' > file  // para escribir
+	cat // para leer mensaje
+	rm -rf carpeta // eliminar carpeta
+	mkdir branches && cd branches // dos acciones en una linea
+
+	Pass: Benja2014
+	dddd
